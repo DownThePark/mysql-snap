8636e5b17b96db06a00b7ed359f8fa4ca7e148ad
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-01-25 10:59:14 -0500

    Move data initialization into first_run.sh

--- snap/local/snap/command-chain/first_run.sh
+++ snap/local/snap/command-chain/first_run.sh
@@ -1,30 +1,46 @@
 #!/bin/bash
 
-#
-# Initialize contents for $SNAP_COMMON
-#
-if [ ! -d $SNAP_COMMON/data ] ; then
-  mkdir $SNAP_COMMON/data
-fi
-
-#
 # Initialize contents for $SNAP_DATA
-#
-if [ ! -d $SNAP_DATA/etc ] ; then
-  mkdir $SNAP_DATA/etc
-fi
+init_snap_data() {
+  if [ ! -d $SNAP_DATA/etc ] ; then
+    mkdir $SNAP_DATA/etc
+  fi
 
-if [ ! -d $SNAP_DATA/log ] ; then
-  mkdir $SNAP_DATA/log
-  touch $SNAP_DATA/log/mysql.log
-  touch $SNAP_DATA/log/mysql_error.log
-fi
+  if [ ! -d $SNAP_DATA/log ] ; then
+    mkdir $SNAP_DATA/log
+    touch $SNAP_DATA/log/mysql.log
+    touch $SNAP_DATA/log/mysql_error.log
+  fi
 
-if [ ! -d $SNAP_DATA/run ] ; then
-  mkdir $SNAP_DATA/run
-fi
+  if [ ! -d $SNAP_DATA/run ] ; then
+    mkdir $SNAP_DATA/run
+  fi
 
-if [ -z "$(ls -A $SNAP_DATA/etc)" ] ; then
-  cp $SNAP/etc/mysql.cnf $SNAP_DATA/etc/
-  cp $SNAP/etc/mysqld.cnf $SNAP_DATA/etc/
-fi
+  if [ -z "$(ls -A $SNAP_DATA/etc)" ] ; then
+    cp $SNAP/etc/mysql.cnf $SNAP_DATA/etc/
+    cp $SNAP/etc/mysqld.cnf $SNAP_DATA/etc/
+  fi
+}
+
+# Initialize contents for $SNAP_COMMON
+init_snap_common() {
+  if [ ! -d $SNAP_COMMON/data ] ; then
+    mkdir $SNAP_COMMON/data
+  fi
+}
+
+# Initialize MySQL data directory
+init_mysql_data() {
+  if [ -z $(ls -A $SNAP_COMMON/data) ] ; then
+    $SNAP/usr/bin/mysqld --defaults-file=$SNAP/etc/my.cnf --initialize --console
+    chmod -R 700 $SNAP_COMMON/data
+    if [ ! -f $SNAP_COMMON/data/mysql.ibd ] ; then
+      echo "Error: MySQL was unable to initialize the data directory at $SNAP_COMMON/data"
+      exit 1
+    fi
+  fi
+}
+
+init_snap_data
+init_snap_common
+init_mysql_data
