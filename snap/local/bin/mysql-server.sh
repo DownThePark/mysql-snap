7b985757738f2c1996e97bca3f82aa6fb4c54f79
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-01-25 10:40:25 -0500

    Update data initialization message
    Add information showing location of data, add data initialization status

--- snap/local/bin/mysql-server.sh
+++ snap/local/bin/mysql-server.sh
@@ -5,10 +5,15 @@ export DATA_DIR=$SNAP_COMMON/data
 #
 # Initialize MySQL
 #
-if [ -z "$(ls -A $DATA_DIR)" ] ; then
-  echo "Initializing MySQL data directory..."
+if [ -z $(ls -A $DATA_DIR) ] ; then
+  echo "Initializing MySQL data directory at $SNAP_COMMON/data"
   $SNAP/usr/bin/mysqld --defaults-file=$SNAP/etc/my.cnf --initialize --console
   chmod -R 700 $DATA_DIR
+  if [ -f $DATA_DIR/mysql.ibd ] ; then
+    echo "Initialization complete!" ;
+  else
+    echo "Error: MySQL was unable to initialize the data directory!"
+  fi
 fi
 
 #
