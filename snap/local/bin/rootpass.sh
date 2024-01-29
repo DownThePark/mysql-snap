b657aab79116caede8bd617be8dd9b6a6e528af4
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-01-29 01:33:01 -0500

    Update message for MySQL root password retrieval

--- snap/local/bin/rootpass.sh
+++ snap/local/bin/rootpass.sh
@@ -2,15 +2,17 @@
 
 if [ "$(id -u)" -ne 0 ]; then echo "Error! Please re-run this script as root." >&2; exit 1; fi
 
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
 echo ""
 cat $SNAP_DATA/log/error.log | grep -o "A temporary password is generated for root@localhost:.*"
-echo "
---- IMPORTANT ---
-This password is stored in plaintext on your filesystem. Therefore, it is
-paramount that you change it immediately! You can do so using the following
-commands (replace YOUR_NEW_PASS with a strong password):
+echo -e "
+--- Note ---
+MySQL will prevent you from using the MySQL server until you change this password.
+To do so, you can use the following two commands (don't forget to change YOUR_NEW_PASS
+with a strong password):
 
-- mysql-strict.mysql -u root -p
-- ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'YOUR_NEW_PASS';
-- exit
+${GREEN}$ ${NOCOLOR}_NAME_.mysql -u root -p
+${GREEN}mysql> ${NOCOLOR}ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'YOUR_NEW_PASS';
 "
