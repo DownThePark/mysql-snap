1fee979f8447ccff1bba790d6e767b779fd0efa9
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-01-28 20:50:30 -0500

    Run 'echo' with quotes for multiple lines
    Rather than issuing echo for each line, it's simpler to just run echo once with the output surrounded by quotes

--- snap/local/bin/rootpass.sh
+++ snap/local/bin/rootpass.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ "$(id -u)" -ne 0 ]; then echo "Please re-run this program as root." >&2; exit 1; fi
+if [ "$(id -u)" -ne 0 ]; then echo "Error! Please re-run this script as root." >&2; exit 1; fi
 
 echo ""
 cat $SNAP_DATA/log/error.log | grep -o "A temporary password is generated for root@localhost:.*"
-echo ""
-echo "--- IMPORTANT ---"
-echo "This password is stored in plaintext on your filesystem. Therefore, it is"
-echo "paramount that you change it immediately! You can do so using the following"
-echo "commands (replace 'YOUR_PASS' with a strong password):"
-echo ""
-echo "- mysql-strict.mysql -u root -p"
-echo "- ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'YOUR_PASS';"
-echo "- exit"
-echo "----------------"
+echo "
+--- IMPORTANT ---
+This password is stored in plaintext on your filesystem. Therefore, it is
+paramount that you change it immediately! You can do so using the following
+commands (replace YOUR_NEW_PASS with a strong password):
+
+- mysql-strict.mysql -u root -p
+- ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'YOUR_NEW_PASS';
+- exit
+"
