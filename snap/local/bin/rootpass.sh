bffcbc0041a5243611613612dfc3f0e13661ea62
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-03-11 12:00:39 -0400

    Update rootpass.sh

--- snap/local/bin/rootpass.sh
+++ snap/local/bin/rootpass.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-if [ "$(id -u)" -ne 0 ]; then echo "Error: This command requires root privileges!" >&2; exit 1; fi
+if [ "$(id -u)" -ne 0 ]; then echo "Error: This command requires root privileges." >&2; exit 1; fi
 
-echo ""
 cat $SNAP_DATA/log/error.log | grep -o "A temporary password is generated for root@localhost:.*"
 echo -e "
-MySQL will prevent itself from being used until you change this password.
-To update it, start by logging to the MySQL server:
+To update this password, start by logging to the MySQL server:
 
 > mysql-strict.mysql -u root -p
 
 Afterwards, execute the following SQL statement (replace YOUR_NEW_PASS below with a strong password):
 
-> ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'YOUR_NEW_PASS';
-"
+> ALTER USER 'root'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'YOUR_NEW_PASS';"
