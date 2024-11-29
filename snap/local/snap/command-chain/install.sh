f63d5fc156a2a0f86dd0a765f241e0fdb372b6a6
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-11-29 17:26:31 -0500

    Change run group from root to snap_daemon

--- snap/local/snap/command-chain/install.sh
+++ snap/local/snap/command-chain/install.sh
@@ -5,7 +5,7 @@ init_snap_data() {
   # Configuration
   mkdir $SNAP_DATA/etc
   cp $SNAP/etc/mysqld.cnf $SNAP_DATA/etc
-  chown -R snap_daemon:root $SNAP_DATA/etc
+  chown -R snap_daemon:snap_daemon $SNAP_DATA/etc
 
   # Logs
   mkdir $SNAP_DATA/log
@@ -14,17 +14,17 @@ init_snap_data() {
   touch $SNAP_DATA/log/mysql-bin.log
   touch $SNAP_DATA/log/mysql-slow.log
   chmod -R o-rwx $SNAP_DATA/log
-  chown -R snap_daemon:root $SNAP_DATA/log
+  chown -R snap_daemon:snap_daemon $SNAP_DATA/log
 
   # Sockets
   mkdir $SNAP_DATA/run
-  chown -R snap_daemon:root $SNAP_DATA/run
+  chown -R snap_daemon:snap_daemon $SNAP_DATA/run
 }
 
 # Initialize contents for $SNAP_COMMON
 init_snap_common() {
   mkdir $SNAP_COMMON/data
-  chown snap_daemon:root $SNAP_COMMON/data
+  chown snap_daemon:snap_daemon $SNAP_COMMON/data
   $SNAP/bin/setpriv.sh $SNAP/usr/bin/mysqld --defaults-file=$SNAP/etc/my.cnf --initialize --console
   if [ ! -f $SNAP_COMMON/data/mysql.ibd ] ; then
     echo "Error: MySQL was unable to initialize the data directory at $SNAP_COMMON/data"
