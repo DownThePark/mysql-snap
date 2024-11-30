d9b366b66d539fdf82f66b456c97379337a3a64f
DownThePark <28867175+DownThePark@users.noreply.github.com>
2024-11-30 14:49:09 -0500

    Initialize MySQL without root password, and enable auth_socket plugin
    This is in-line with how the Ubuntu/Debian packages initialize the MySQL data directory, and authenticate the MySQL root user.

--- snap/local/snap/command-chain/install.sh
+++ snap/local/snap/command-chain/install.sh
@@ -23,15 +23,33 @@ init_snap_data() {
 
 # Initialize contents for $SNAP_COMMON
 init_snap_common() {
+  # Create directory for storing data and set permissions
   mkdir $SNAP_COMMON/data
   chown snap_daemon:root $SNAP_COMMON/data
-  $SNAP/bin/setpriv.sh $SNAP/usr/bin/mysqld --defaults-file=$SNAP/etc/my.cnf --initialize --console
+
+  # Initialize data directory
+  $SNAP/bin/setpriv.sh $SNAP/usr/bin/mysqld --defaults-file=$SNAP/etc/my.cnf --initialize-insecure
+
+  # Throw error if data directory not initialized
   if [ ! -f $SNAP_COMMON/data/mysql.ibd ] ; then
     echo "Error: MySQL was unable to initialize the data directory at $SNAP_COMMON/data"
     exit 1
   fi
-  $SNAP/bin/setpriv.sh chmod -R go-rwx $SNAP_COMMON/data
+
+  # Remove "group" and "other" permission access from data directory
+  chmod go-rwx $SNAP_COMMON/data
+}
+
+# Enable auth socket plugin
+enable_auth_socket() {
+  local init_file="$(mktemp)"
+  echo "INSTALL PLUGIN auth_socket SONAME 'auth_socket.so';" >> "$init_file"
+  echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH 'auth_socket';" >> "$init_file"
+  echo "SHUTDOWN;" >> "$init_file"
+  chown snap_daemon:root "$init_file"
+  $SNAP/bin/setpriv.sh $SNAP/usr/bin/mysqld --user=snap_daemon --skip-networking --skip-mysqlx --init-file="$init_file"
 }
 
 init_snap_data
 init_snap_common
+enable_auth_socket
